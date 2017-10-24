a7da4157792ef31af67d67a6d399a803f5d7c284
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-24 11:35:14 -0700

    Adds function to take a list of timezones and returns a list of times. #1

--- public/main.js
+++ public/main.js
@@ -8,31 +8,6 @@ const times = areas =>
       .format('hh:mm:ssA')
   }))
 
-const render = time =>
-  (($time, $value) => {
-    $time.classList.add('time')
-    $value.textContent = `${time.zone} ${time.time}`
-    $value.classList.add('value')
-    return $time.appendChild($value).parentNode
-  })(document.createElement('div'), document.createElement('div'))
-
 const timezones = () => {
   return fetch('/clock/times').then(res => res.json())
 }
-
-const tick = () => {
-  const zones = timezones()
-  setInterval(() => {
-    zones
-      .then(times)
-      .then(data => data.map(render))
-      .then(data => {
-        document.querySelector('#times').innerHTML = ''
-        data.forEach(element => {
-          document.querySelector('#times').appendChild(element)
-        })
-      })
-  }, 16)
-}
-
-tick()
